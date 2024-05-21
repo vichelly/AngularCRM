dc488beba681fb02e1c8afa9c8c20c25316402d1
Vitor Felício <vitor.felicio@EMPAGISPM0114.local>
2024-05-21 11:20:00 -0300

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { HomeComponent } from './home/home.component';
+import { CreateCardComponent } from './components/cards/create-card/create-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, FormsModule, HomeComponent, CreateCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularCRM';
---
+++ src/app/components/cards/create-card/create-card.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-card',
+  standalone: true,
+  templateUrl: './create-card.component.html',
+  styleUrls: ['./create-card.component.css'],
+  imports: [FormsModule]
+})
+export class CreateCardComponent {
+  card = {
+    id: '1',
+    content: 'TESTE'
+  };
+
+  createCard() {
+    console.log('Card created:', this.card);
+  }
+}
