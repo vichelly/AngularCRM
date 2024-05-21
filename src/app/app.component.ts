ded2661e34999c588a44b5811f304267ee762643
Vitor Felício <vitor.felicio@EMPAGISPM0114.local>
2024-05-21 12:17:50 -0300

    rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateCardComponent } from './components/cards/create-card/create-card.component';
 import { FormsModule } from '@angular/forms';
+import { CardListComponent } from './components/cards/card-list/card-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, HomeComponent, CreateCardComponent],
+  imports: [RouterOutlet, FormsModule, HomeComponent, CreateCardComponent, CardListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CreateCardComponent } from './components/cards/create-card/create-card.component';
+import { CardListComponent } from './components/cards/card-list/card-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'listCard',
+        pathMatch: 'full',
+    },
+    {
+        path: 'createCard',
+        component: CreateCardComponent
+    },
+    {
+        path: 'listCard',
+        component: CardListComponent
+    },
+];
